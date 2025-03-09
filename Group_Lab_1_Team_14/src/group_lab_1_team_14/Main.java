7900803d4023812a87d5c5ea4d803a0b2f4470f9
linweih-alvin <lin.weih@northeastern.edu>
2025-03-09 17:04:57 -0400

    add Main.java as main function

---
+++ Group_Lab_1_Team_14/src/group_lab_1_team_14/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group_lab_1_team_14;
+
+/**
+ *
+ * @author linweihong
+ */
+public class Main {
+    public static void main(String[] args) {
+        Member1.main(args);
+        Member2.main(args);
+        Member3.main(args);
+    }
+}
+
